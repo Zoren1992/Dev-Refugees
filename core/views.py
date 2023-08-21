63b5739beb3565887229ff92e141723c2f9354dd
SamuelBai0910 <baisimo2022@163.com>
2023-08-21 18:38:22 +1000

    finish dynamic nav bar and log&signup function

--- core/views.py
+++ core/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
 from . models import User, Answer, Question, Tag
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 
 def home(request):
@@ -9,3 +11,21 @@ def home(request):
 
 def contact(request):
   return render(request, 'core/contact.html')
+
+def signup(request):
+  error_message = ''
+  form = UserCreationForm(request.POST)
+  if request.method == 'POST':
+    # This is how to create a 'user' form object
+    # that includes the data from the browser
+    if form.is_valid():
+      # This will add the user to the database
+      user = form.save()
+      # This is how we log a user in via code
+      login(request, user)
+      return redirect('home')
+    else:
+      error_message = 'Invalid sign up - try again'
+  # A bad POST or a GET request, so render signup.html with an empty form
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
