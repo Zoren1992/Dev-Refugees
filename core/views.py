0c5db9b8df087d114a5d4f7ba1d3ce3608864341
SamuelBai0910 <baisimo2022@163.com>
2023-08-21 15:28:56 +1000

    fix home and contact page path

--- core/views.py
+++ core/views.py
@@ -3,3 +3,9 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
 from . models import User, Answer, Question, Tag
 
+
+def home(request):
+  return render(request, 'core/index.html')
+
+def contact(request):
+  return render(request, 'core/contact.html')
--- puddle/urls.py
+++ puddle/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
-# from core.views import index, contact
+# from core.views import
 
 urlpatterns = [
-    # path("", index, name="index"),
-    # path("contact/", contact, name="contact"),
     path('admin/', admin.site.urls),
+    path('', include('core.urls')),
 ]
